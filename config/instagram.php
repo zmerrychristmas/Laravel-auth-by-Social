660d19063f408867b502f730f298f5ec068ac1f7
Nguyen Huy Binh <binhnh@beetsoft.com.vn>
2017-04-03 17:10:52 +0700

    Init Oauth

---
+++ config/instagram.php
@@ -0,0 +1,13 @@
+<?php
+/***** initialize class
+* $instagram = new Instagram(array(
+*   'apiKey'      => 'YOUR_APP_KEY',
+*   'apiSecret'   => 'YOUR_APP_SECRET',
+*   'apiCallback' => 'YOUR_APP_CALLBACK' // must point to process url
+* ));
+***********************************/
+return [
+    'apiKey'    =>  '',
+    'apiSecret' =>  '',
+    'apiCallback'   =>  ''
+];
